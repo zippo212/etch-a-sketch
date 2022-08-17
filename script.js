56ae8a741bcf73f83b1c0faf8a996e3b02d4e9b7
Oleg Maizer <zippooleg3@gmail.com>
2022-08-17 19:42:01 +0300

    Finshed style and updated functionality
    added background img buttons style and updated the functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container')
 const slider = document.querySelector('.slider')
 const value = document.querySelector('.value')
+const btns = document.querySelectorAll('button')
 let root = document.querySelector(':root')
 
 
@@ -11,18 +12,19 @@ let i = 0
     while (i < (num*num) ){
         const squareDiv = document.createElement('div')
         squareDiv.className = 'square'
-        squareDiv.addEventListener('mousedown', changeColor)
-        squareDiv.addEventListener('mouseover', changeColor)
+        squareDiv.addEventListener('mousedown', changeColor) // listen to mousedown and mouseover  events on the square
+        squareDiv.addEventListener('mouseover', changeColor) // and call changeColor each time
         container.append(squareDiv)
         i++
     }
     root.style.setProperty('--grid-layout', `repeat(${num}, 1fr)`)
 }
 
+// listen to change in slider and run function accordingly 
 slider.addEventListener('mouseup',() => createLayout(+slider.value))
 slider.addEventListener('mousemove',() => updateValue(+slider.value))
 
-
+// update the value of the slider in real time
 function updateValue(show) {
     value.textContent = `${show} x ${show}`
 }
@@ -39,33 +41,65 @@ function getRgb () {
 }
 
 let currentColor;
+let active;
 
-document.addEventListener('click',(e) => {
-    id = e.target.id
-    if (id === 'rgb') currentColor = `rgb`
-    if (id === 'e') currentColor = 'white'
-    if (id === 'clear') clear()
+// add Eventlistener to each button and run function with his id 
+btns.forEach(btn => {
+    btn.addEventListener('click',(e) => {
+        active = e.target.id
+        colorChange(active);
+        clicked(active);
+    })
 })
 
+// change currentColor value based on the button clicked
+function colorChange(active) {
+    switch (active) {
+        case 'black':
+            currentColor = 'black'
+        break;
+        case 'e':
+            currentColor = 'antiquewhite'
+        break;
+        case 'clear':
+            clear()
+        break;
+    }
+}
+
+// add class to only the active button
+function clicked(active) {
+    btns.forEach(btn => {
+        if (btn.id === active) {
+            btn.className = 'active'
+        } else btn.className = ''
+    })
+}
+
+// clear the grid 
 function clear () {
     createLayout(+slider.value)
 }
 
+// update mouseDown value if mouse is done = true 
 let mouseDown = false
-container.addEventListener('mousedown',() => mouseDown = true);
-container.addEventListener('mouseup',() => mouseDown = false);
+document.addEventListener('mousedown',() => mouseDown = true);
+document.addEventListener('mouseup',() => mouseDown = false);
 
-
+// change color of grid elements if condition is true
 function changeColor (e) {
     if (e.type === 'mouseover' && !mouseDown) return
-    if (currentColor === 'rgb') e.target.style.backgroundColor = `rgb(${getRgb()}, ${getRgb()}, ${getRgb()})`
-    if (currentColor === 'white') e.target.style.backgroundColor = 'white'
-    if (!currentColor) e.target.style.backgroundColor = 'black'
+    if (active === 'rgb') e.target.style.backgroundColor = `rgb(${getRgb()}, ${getRgb()}, ${getRgb()})`
+    else e.target.style.backgroundColor = currentColor
 }
 
+// prevent mouse blocking
+container.onmousedown = (e) => {
+    mouseDown = true; 
+    e.preventDefault();
+};
 
-
-
+// load at start default value
 window.onload = () => {
     createLayout(+slider.value);
     updateValue(+slider.value)
